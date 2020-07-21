3b854b5dc427e10e0949968ebf6537360400d359
Mario Lorenzo <mariolorenzo213@gmail.com>
2020-07-21 15:12:24 +0800

    Updates

---
+++ precache-manifest.fad4c28f33a36f8d2b1e876c19879bdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f978ef0e49b927eda30e",
+    "url": "/dotaNUBS/static/css/main.906a3b0d.chunk.css"
+  },
+  {
+    "revision": "f978ef0e49b927eda30e",
+    "url": "/dotaNUBS/static/js/main.faae5438.chunk.js"
+  },
+  {
+    "revision": "480993ff76bce720e6a3",
+    "url": "/dotaNUBS/static/js/runtime~main.a26ec5f6.js"
+  },
+  {
+    "revision": "1a95cf2a8d60dfb40551",
+    "url": "/dotaNUBS/static/js/2.426aa5fe.chunk.js"
+  },
+  {
+    "revision": "3c2a6a59e312e94019aea8f5320624f5",
+    "url": "/dotaNUBS/static/media/bg.3c2a6a59.jpg"
+  },
+  {
+    "revision": "930b2e6ff56c12069ab38860677f95c4",
+    "url": "/dotaNUBS/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e5dd9427132a26ee396f808e8e9b8b3a.js"
+  "/dotaNUBS/precache-manifest.fad4c28f33a36f8d2b1e876c19879bdf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/dotaNUBS/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
